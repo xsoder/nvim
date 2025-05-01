44fb82b7d37769383ea3ee715e62bb6e97db498b
csodelinux <csodelinux@gmail.com>
2025-05-01 17:04:13 -0400

    feat : added coloring

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,52 +1,89 @@
+function ColorMyPencils(color)
+	color = color or "rose-pine-moon"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 return {
-    {
-        "rose-pine/neovim",
-        config = function()
-            vim.cmd([[
-				colorscheme rose-pine
 
-				" Main backgrounds
-				highlight Normal guibg=NONE ctermbg=NONE
-				highlight NormalNC guibg=NONE ctermbg=NONE
-				highlight NonText guibg=NONE ctermbg=NONE
+	{
+		"erikbackman/brightburn.vim",
+	},
 
-				" Line numbers
-				highlight LineNr guibg=NONE ctermbg=NONE
-				highlight CursorLineNr guibg=NONE ctermbg=NONE
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		opts = {},
+		config = function()
+			ColorMyPencils()
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		name = "gruvbox",
+		config = function()
+			require("gruvbox").setup({
+				terminal_colors = true, -- add neovim terminal colors
+				undercurl = true,
+				underline = false,
+				bold = true,
+				italic = {
+					strings = false,
+					emphasis = false,
+					comments = false,
+					operators = false,
+					folds = false,
+				},
+				strikethrough = true,
+				invert_selection = false,
+				invert_signs = false,
+				invert_tabline = false,
+				invert_intend_guides = false,
+				inverse = true, -- invert background for search, diffs, statuslines and errors
+				contrast = "", -- can be "hard", "soft" or empty string
+				palette_overrides = {},
+				overrides = {},
+				dim_inactive = false,
+				transparent_mode = false,
+			})
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		config = function()
+			require("tokyonight").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+				transparent = true, -- Enable this to disable setting the background color
+				terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+				styles = {
+					-- Style to be applied to different syntax groups
+					-- Value is any valid attr-list value for `:help nvim_set_hl`
+					comments = { italic = false },
+					keywords = { italic = false },
+					-- Background styles. Can be "dark", "transparent" or "normal"
+					sidebars = "dark", -- style for sidebars, see below
+					floats = "dark", -- style for floating windows
+				},
+			})
+		end,
+	},
 
-				" Folds
-				highlight Folded guibg=NONE ctermbg=NONE
-				highlight FoldColumn guibg=NONE ctermbg=NONE
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				disable_background = true,
+				styles = {
+					italic = false,
+				},
+			})
 
-				" Sign column (like git signs, diagnostics)
-				highlight SignColumn guibg=NONE ctermbg=NONE
-				" Vert splits
-				highlight VertSplit guibg=NONE ctermbg=NONE
-
-				" Tab line
-				highlight TabLine guibg=NONE ctermbg=NONE
-				highlight TabLineFill guibg=NONE ctermbg=NONE
-				highlight TabLineSel guibg=NONE ctermbg=NONE
-
-				" Popup menus
-				highlight Pmenu guibg=NONE ctermbg=NONE
-				highlight PmenuSel guibg=NONE ctermbg=NONE
-
-				" Floating windows
-				highlight NormalFloat guibg=NONE ctermbg=NONE
-				highlight FloatBorder guibg=NONE ctermbg=NONE
-
-				" LSP-related floating things
-				highlight LspFloatWinNormal guibg=NONE ctermbg=NONE
-				highlight LspFloatWinBorder guibg=NONE ctermbg=NONE
-
-				" Telescope (if you use it)
-				highlight TelescopeNormal guibg=NONE ctermbg=NONE
-				highlight TelescopeBorder guibg=NONE ctermbg=NONE
-
-				" WhichKey (if you use it)
-				highlight WhichKeyFloat guibg=NONE ctermbg=NONE
-			]])
-        end,
-    },
+			ColorMyPencils()
+		end,
+	},
 }
